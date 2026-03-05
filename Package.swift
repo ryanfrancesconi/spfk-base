5999891ec8be48c4ab75e482029df74079df9739
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-05 07:43:54 -0800

    up package requirements to current minimums

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.5"),
         .package(url: "https://github.com/orchetect/swift-extensions", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-numerics", from: "1.1.1"),
         .package(url: "https://github.com/apple/swift-collections", from: "1.3.0"),
